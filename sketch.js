2adc04ac72d889293c1e0e4ca7d40174c719c3be
Yasmin Elgerf <qf22lq@LABS-M64050MW.local>
2026-06-02 14:46:26 -0400

    Tried using finger to draw on the screen

--- sketch.js
+++ sketch.js
@@ -2,8 +2,15 @@
 // I took bits of this from the ML5 website 
 // Michael Palumbo
 
+
+let video;
 let handPose;
 let hands = [];
+
+//let drawPoint = [0, 0];
+let cutoff = 0;
+
+let drawCoords = [];
 
 function preload() {
   handPose = ml5.handPose();
@@ -11,20 +18,54 @@ function preload() {
 
 function setup() {
   createCanvas(640, 480);
-  let video = createCapture(VIDEO);
+  video = createCapture(VIDEO);
   video.hide();
-  handPose.detectStart(video, function(results) {
+  handPose.detectStart(video, function (results) {
     hands = results;
   });
 }
 
 function draw() {
-  background(0);
+  //background(0);
+  if (video) {
+    image(video, 0, 0);
+  }
+  
   for (let hand of hands) {
     for (let kp of hand.keypoints) {
-      fill(0, 255, 0);
-      noStroke();
-      circle(kp.x, kp.y, 10);
+      if (kp.name == "index_finger_pip") {
+        drawPoint[0] = kp.x;
+        drawPoint[1] = kp.y;
+
+        drawCoords.push([kp.x, kp.y]);
+
+        circle(kp.x, kp.y, 10);
+      } else if (kp.name == 'index_finger_pip') {
+        cutoff = kp.y;
+      }
+      fill(0);
+
+
+      if (drawPoint[1] > cutoff) {
+        console.log('cutoff');
+
+        fill(255, 255, 255);
+        noStroke();
+        circle(drawPoint[0], drawPoint[1], 13);
+
+      }
+
     }
+  }
+
+  for (let point of drawCoords) {
+    fill(0, 255, 0);
+    noStroke();
+    circle(point[0], point[1], 10);
+  }
+  console.log(drawCoords);
+
+  if (hands.length > 1) {
+    console.log('two hands!');
   }
 }
